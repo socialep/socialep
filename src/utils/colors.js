4af4649d44fe2189c114442423e6788fe8f8a4e4
Marcos Marques <marcoshenrique150325@gmail.com>
2020-03-30 14:08:35 -0300

    Implementa a tela de seleção de interesses

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -3,11 +3,18 @@ export const colorPrimary = "#6C63FF";
 //backgrounds
 export const colorCarousel = "#CBCBCB";
 export const colorSignInHeader = "#404040";
+export const colorLightGrayBg = "#F8F8F8";
+export const colorInterestCardBg = "#FFFFFF";
 
 //fonts
 export const fontColor = "#707070";
 export const fontColorLight = "#9A9A9A";
+export const colorError = "red";
 
 //buttons
-export const colorSignInGoogle = "#FF6E6E"
-export const colorSignInFacebook = "#6C63FF"
+export const colorSignInGoogle = "#FF6E6E";
+export const colorSignInFacebook = "#6C63FF";
+
+//icons
+export const colorSelected = "#12B2A0";
+export const colorUnselected = "#B4B4B4";
--- src/utils/strings.js
+++ src/utils/strings.js
@@ -15,7 +15,19 @@ export default strings = language => {
         signIn: "ENTRAR",
         signInGoogle: "Conectar com Google",
         signInFacebook: "Conectar com Facebook",
-        useAndPrivacy: "Uso e Privacidade"
+        useAndPrivacy: "Uso e Privacidade",
+        //
+        environment: "Meio Ambiente",
+        education: "Educação",
+        sports: "Esportes",
+        humanRights: "Direitos Humanos",
+        animals: "Animais",
+        health: "Saúde",
+        howYWHelp: "Quais causas você quer ajudar?",
+        //
+        continue: "Continuar",
+        //
+        mustChooseAtLeastOne: "Você precisa escolher ao menos um interesse"
       };
   }
 };
