96d51a8f7834f114acd22f4a7e33e0ede805479e
Marcos Marques <marcoshenrique150325@gmail.com>
2020-05-02 12:06:25 -0300

    Cria tela principal

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,16 +1,79 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { View, ScrollView } from "react-native";
+import styles from "./styles";
 
-export default function App() {
+//Components
+import CustomAppBar from "../../components/CustomAppBar";
+import Loading from "../../components/Loading";
+import OpportunityCard from "../../components/OpportunityCard";
+
+//Redux
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+//view-carousel | view-agenda | more-vert
+
+const index = (props) => {
+  const {
+    ui: { strings, loading },
+  } = props;
+
+  const [mode, setMode] = useState("carrousel");
+
+  const opportunities = [
+    {
+      rating: 3.5,
+      photo:
+        "https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+      name: "Oportunidade de Teste",
+      description:
+        "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore.",
+      liked: true,
+    },
+    {
+      rating: 3.5,
+      photo:
+        "https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80",
+      name: "Oportunidade de Teste",
+      description:
+        "Oportunidade de Teste Oportunidade de Teste Oportunidade de Teste",
+      liked: true,
+    },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>HOME</Text>
+      <CustomAppBar
+        actions={["view-carousel", "more-vert"]}
+        onSecondaryAction={() => console.log("onSecondaryAction")}
+        onThirdAction={() => console.log("onThirdAction")}
+        title={strings.home}
+      />
+      {loading ? (
+        <Loading />
+      ) : (
+        <ScrollView>
+          {opportunities.map((opp) => (
+            <OpportunityCard
+              opportunity={opp}
+              handleFavPressed={() => console.log("favorited")}
+              btnPressed={() => console.log("pressed")}
+            />
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
+const mapStateToProps = (state) => ({
+  ui: state.ui,
 });
+
+const mapDispatchToProps = {};
+
+index.propTypes = {
+  ui: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(index);
--- src/screens/home/styles.js
+++ src/screens/home/styles.js
@@ -1,1 +1,9 @@
+import { StyleSheet } from "react-native";
+import { colorInterestCardBg } from "../../utils/colors";
 
+export default styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colorInterestCardBg,
+  },
+});
