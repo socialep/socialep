ef1a5818c880ae1b17123c0549903503faeb04ca
Marcos Marques <marcoshenrique150325@gmail.com>
2020-05-02 12:06:57 -0300

    Adiciona algumas ações

--- src/redux/actions/uiActions.js
+++ src/redux/actions/uiActions.js
@@ -1,6 +1,6 @@
 import axiosInstance from "../../config/axiosInstance";
 import { SET_STRINGS, TOGGLE_LOADING } from "../types";
 
-export const setStrings = data => dispatch => {
+export const setStrings = (data) => (dispatch) => {
   dispatch({ type: SET_STRINGS, payload: data });
 };
---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,8 @@
+import axiosInstance from "../../config/axiosInstance";
+import { TOGGLE_LOADING } from "../types";
+
+export const signInGoogle = () => async (dispatch) => {};
+
+export const signInFaceBook = () => async (dispatch) => {};
+
+export const signOut = () => async (dispatch) => {};
