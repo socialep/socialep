a20531691cd617f71e74f20b8ad27c83a0903b67
Marcos Marques <marcoshenrique150325@gmail.com>
2020-05-05 16:29:35 -0300

    Estiliza AppBar

--- src/components/CustomAppBar.js
+++ src/components/CustomAppBar.js
@@ -1,31 +1,53 @@
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 //MATERIAL BREAD
-import { Appbar } from "material-bread";
+import { Appbar, IconButton } from "material-bread";
 
 //UTILS
-import { colorPrimary } from "../utils/colors";
+import {
+  colorPrimary,
+  colorLightGrayBg,
+  colorUnselected,
+} from "../utils/colors";
+
+const styles = StyleSheet.create({
+  actionsContainer: {
+    flexDirection: "row",
+  },
+  lblTitle: {
+    fontSize: 20,
+    color: colorUnselected,
+    fontWeight: "700",
+  },
+});
 
 const CustomAppBar = (props) => {
   const { mode, actions, title } = props;
 
-  const actionsItens = actions
-    ? actions.map((action, index) => ({
-        name: action,
-        onPress: index == 0 ? props.onSecondaryAction : props.onThirdAction,
-      }))
-    : [];
-
   return (
-    <Appbar
-      barType="dense"
-      color={colorPrimary}
-      title={title ? title : ""}
-      style={styles.appBar}
-      navigation={mode ? mode : null}
-      onNavigation={props.onNavigation}
-      actionItems={actionsItens}
-    />
+    <Appbar color={colorLightGrayBg} elevation={10}>
+      {mode && (
+        <Icon
+          name={mode}
+          color={colorUnselected}
+          size={32}
+          onPress={props.onNavigation}
+        />
+      )}
+      <Text style={styles.lblTitle}>{title}</Text>
+      <View style={styles.actionsContainer}>
+        {actions.map((action, index) => (
+          <IconButton
+            key={index}
+            name={action}
+            color={colorUnselected}
+            size={32}
+            onPress={index == 0 ? props.onSecondaryAction : props.onThirdAction}
+          />
+        ))}
+      </View>
+    </Appbar>
   );
 };
 
