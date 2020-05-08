da7ca785460b24b4ca1da8f29625ffff0fa2459b
Marcos Marques <marcoshenrique150325@gmail.com>
2020-05-08 10:37:01 -0300

    Implementa tela de favoritos

--- src/screens/favorites/index.js
+++ src/screens/favorites/index.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 import styles from "./styles";
 
 //Components
 import CustomAppBar from "../../components/CustomAppBar";
+import OpportunityListItem from "../../components/OpportunityListItem";
+import Loading from "../../components/Loading";
 
 //Redux
 import PropTypes from "prop-types";
@@ -13,11 +15,27 @@ import { connect } from "react-redux";
 
 const index = (props) => {
   const {
-    ui: { strings },
+    ui: { strings, favorites, loading },
+    navigation,
   } = props;
+
   return (
     <View style={styles.container}>
       <CustomAppBar title={strings.favorites} />
+      {loading ? (
+        <Loading />
+      ) : (
+        <ScrollView>
+          {favorites.map((opp, index) => (
+            <OpportunityListItem
+              key={index}
+              opportunity={opp}
+              handleFavPressed={() => console.log("favorited")}
+              btnPressed={() => navigation.push("Opportunity", { opp })}
+            />
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
 };
